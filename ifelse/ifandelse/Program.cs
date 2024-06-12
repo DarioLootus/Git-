799c0b7017b4d34349cab2b7f7b2678684bf4a91
Opilane TTHK <opilane@tthk.ee>
2024-06-12 11:36:19 +0300

    Ifelse

---
+++ ifelse/ifandelse/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ifandelse
+{
+    internal class Program
+    {
+        private static int firstNumber;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If and else nesting");
+
+            int first = 7, second = -23, third = 13;
+            if (first > second)
+            {
+                if (firstNumber > third)
+                {
+                    Console.WriteLine("{0} is the largest", first);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is the largest", third);
+                }
+            }
+            else
+            {
+                if (second > third)
+                {
+                    Console.WriteLine("{0} is the largest", second);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is the largest", third);
+                }
+            }
+        }
+    }
+}
